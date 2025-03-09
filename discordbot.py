36ebc9146ee77cb0605e41ab970fa8d04be4e981
Yosya4476 <yoshi44763328@gmail.com>
2025-03-10 03:11:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -40,13 +40,13 @@ async def on_voice_state_update(member, before, after):
     
     # ボイスチャンネルに入室したときの処理
     if before.channel is None:
-      title = "入室"
+      title = f"#{after.channel.name}に入室"
       message = "ボイスチャンネルに入室したよ！"
       color = 0x7289da
       
     # ボイスチャンネルを退室したときの処理
     elif after.channel is None:
-      title = "退室"
+      title = f"#{before.channel.name}から退室"
       message = "ボイスチャンネルを退室したよ！"
       color = 0xffa500
 
