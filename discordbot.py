ea3e06d5ed2e9db00c992ac13b2f5f5ba91a9807
Yosya4476 <yoshi44763328@gmail.com>
2025-03-09 18:00:02 +0900

    Revert "Update discordbot.py"
    This reverts commit bd2275db71fa413ae1ac6f34aa916e346860724f.

--- discordbot.py
+++ discordbot.py
@@ -23,17 +23,34 @@ async def on_message(message):
   
 # ボイスチャンネルの入退室時に動作する処理 #
 @bot.event
-async def on_voice_state_update(member, before, after): 
-    if (before.channel != after.channel):
-        now = datetime.utcnow()
-        alert_channel = bot.get_channel(952068860629090314)
-        if before.channel is None: 
-            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
-            await alert_channel.send(msg)
-        elif after.channel is None: 
-            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
-            await alert_channel.send(msg)
-    await bot.process_commands(member, before, after)
+async def on_voice_state_update(member, before, after):
+  alert_channel = bot.get_channel(text_id)
+  # ボイスチャンネルを入室または退室したかを判定する
+  if before.channel is not after.channel:
+    
+    # ボイスチャンネルに入室したときの処理
+    if before.channel is None:
+      title = f"#{after.channel.name} に入室"
+      message = "ボイスチャンネルに入室したよ！"
+      color = 0x7289da
+      
+    # ボイスチャンネルを退室したときの処理
+    elif after.channel is None:
+      title = f"#{before.channel.name} を退室"
+      message = "ボイスチャンネルを退室したよ！"
+      color = 0xffa500
+
+    # embedを設定する
+    embed = discord.Embed(title=title,
+                          description=message,
+                          color=color,
+                          timestamp=datetime.utcnow())
+    embed.set_author(name=member.name,
+                     icon_url=member.avatar_url)
+    # embedを送信する
+    await alert_channel.send(embed=embed)
+   
+  await bot.process_commands(member, before, after)
 
   
 # コマンドを入力したときの処理
